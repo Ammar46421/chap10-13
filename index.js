dfb8a3d09bfe90242da6f23b40cd35d4990fae4b
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-03-19 20:48:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ if (x === "Pakistan"){
 if (x != "Pakistan"){
     alert("Incorrect!")
 }
+
+//chap 12
+
+var yourTicketNumber = prompt("Input Your Ticket Number.")
+
+if (yourTicketNumber == 4) {
+    alert("you won rooh afza");
+}
+
+else  {
+    alert("Better luck next time.");
+}
+
+// chap 13
+
+var age = prompt("Enter Your Age.")
+
+if (age <= 17){
+    alert("Pay Rs10.")
+}
+
+if (age >= 18 && age <= 59){
+    alert("Pay Rs20.")
+}
+
+
+if (age >= 60){
+    alert("Free Entry.")
+}
+
